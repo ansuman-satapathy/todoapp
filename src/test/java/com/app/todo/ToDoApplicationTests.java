a4a8fb8e30b4bcad695467402eb83106af5a4b26
ansuman-satapathy <satapathyansuman12@gmail.com>
2024-03-05 20:29:54 +0530

    initial commit

---
+++ src/test/java/com/app/todo/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hnp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HnpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
